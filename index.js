4a50e11a596950bfc556c96b0fb98242d2ea27b8
yesicamilagros <milagroslc654@gmail.com>
2025-04-06 11:32:24 -0500

    primer cambio20

--- index.js
+++ index.js
@@ -36,16 +36,22 @@ app.post("/webhook",(req,res)=>{
     console.log(JSON.stringify(body_param,null,2));
 
     if(body_param.object){
+        console.log("inside body param");
         if(body_param.entry &&
             body_param.entry[0].changes &&
-            body_param.entry[0].changes[0].value.message &&
-            body_param.entry[0].changes[0].value.message[0]
+            body_param.entry[0].changes[0].value.messages &&
+            body_param.entry[0].changes[0].value.messages[0]
         
         ){
 
             let phone_nu_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
             let from = body_param.entry[0].changes[0].value.messages[0].from;
             let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+
+            console.log("phone number"+phone_nu_id);
+            console.log("from"+from);
+            console.log("body param"+msg_body);
 
             axios({
                 method:"POST",
