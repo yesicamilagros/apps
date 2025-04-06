84348e9e6d17d450ac1ec910e68fa03b49b517fb
yesicamilagros <milagroslc654@gmail.com>
2025-04-06 12:14:42 -0500

    primer cambio21

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.post("/webhook",(req,res)=>{
     console.log(JSON.stringify(body_param,null,2));
 
     if(body_param.object){
-        console.log("inside body param");
+        //console.log("inside body param");
         if(body_param.entry &&
             body_param.entry[0].changes &&
             body_param.entry[0].changes[0].value.messages &&
@@ -49,13 +49,13 @@ app.post("/webhook",(req,res)=>{
             let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
 
-            console.log("phone number"+phone_nu_id);
-            console.log("from"+from);
-            console.log("body param"+msg_body);
+           // console.log("phone number"+phone_nu_id);
+            //console.log("from"+from);
+            //console.log("body param"+msg_body);
 
             axios({
                 method:"POST",
-                url:"https://graph.facebook.com/v22.0/"+phone_nu_id+"messages?access_token="+token,
+                url:`https://graph.facebook.com/v22.0/${phone_nu_id}/messages?access_token=${token}`,
                 data:{
                     messaging_product:"whatsapp",
                     to:from,
@@ -64,7 +64,7 @@ app.post("/webhook",(req,res)=>{
                     }
                 },
                 headers:{
-                    "Content-Type":"application/json"
+                     "Content-Type": "application/json"
                 }
             });
             
