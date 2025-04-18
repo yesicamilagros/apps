8fb4bf75d0478e5eff362c039d191df6ae6c6902
yesica milagros leon ccahuana <102852467+yesicamilagros@users.noreply.github.com>
2025-04-18 12:37:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -335,15 +335,19 @@ async function sendasesor(to, phone_number_id) {
                     action: {
                         buttons: [
                             {
-                                "type": "url",
-                                "url": "https://wa.me/1234567890",
-                                "title": "WhatsApp"
-                            },
-                            {
-                                "type": "call",
-                            "phone_number": "+51936696634",
-                            "title": "Llamada"
+                            type: "reply",
+                            reply: {
+                              id: "ases_mensaje",
+                              title: "WhatsApp"
                             }
+                          },
+                          {
+                            type: "reply",
+                            reply: {
+                              id: "ases_llamada",
+                              title: "Llamada"
+                            }
+                          }
                         ]
                     }
                 }
